68c067713dbed7f51f3b71db7640abff6f93b208
LautiLezama <lautarolezama@hotmail.com>
2019-07-20 09:03:32 -0300

    problemas con while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,32 +1,7 @@
 function mostrar()
 {
 
-	var contador=0;
-
-	var numero;
-
-	var numeroDos;
-
-	var suma;
-
-	alert(numero);
-
-	numeroDos=12;
-
-	suma=numero*numeroDos;
-
-	alert(suma);
-
-	suma="lala";
-
-	while(isNaN(suma)){
-
-		alert("Error");
-		suma=prompt("error, reingrese solo numeros");
-	}
-
-	alert("Ingreso correcto"+suma);
-// A VER CAPO, 
+	
 
 
 
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,25 @@
-function mostrar()
-{
+function mostrar() {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 0;
+	var acumulador = 0;
+	var respuesta
+
+	respuesta = "si"
+
+	while (respuesta == "si") {
+
+		contador = prompt("Digite el número");
+
+		acumulador 
+
+		respuesta = prompt("si para seguir");
+
+	}
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / 5;
 
 }//FIN DE LA FUNCIÓN
