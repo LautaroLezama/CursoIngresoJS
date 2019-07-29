af055838433153b07a1ffe67807c12c329843ae7
LautiLezama <lautarolezama@hotmail.com>
2019-07-29 07:25:34 -0300

    resolvi 9 y 10 while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,7 +1,70 @@
 function mostrar()
 {
 
-	
+    var num1;
+    var contadorpos = 0;
+    var contadorneg = 0;
+    var contadorcero = 0;
+    var promediopos = 0;
+    var promedioneg = 0;
+    var cero = 0;
+    var negativo = 0;
+    var positivo = 0;
+    var respuesta = "si";
+    var diferencia = 0;
+    var contadorpares = 0;
+
+
+    while(respuesta == "si"){
+
+    num1 = prompt("Ingresar número");
+
+    num1 = parseInt(num1);
+
+    if (num1 % 2 == 0){
+
+        contadorpares++;
+
+    }
+
+    if (num1 < cero){
+
+    contadorneg ++;
+
+    negativo = negativo + num1;
+
+    promedioneg = negativo / contadorneg;
+
+    } else if(num1 > cero){
+
+    positivo = positivo + num1;
+
+    contadorpos ++;
+
+    promediopos = positivo / contadorpos;
+
+    } else {
+
+    contadorcero ++;
+
+    }
+
+    diferencia = positivo + negativo;    
+
+    respuesta = prompt("si para continuar");
+
+    }
+
+    document.write("La suma de negativos es " + negativo + "<br>");
+    document.write("La suma de positivos es " + positivo + "<br>");
+    document.write("La cantidad de positivos es " + contadorpos + "<br>");
+    document.write("La cantidad de negativos es " + contadorneg + "<br>");
+    document.write("La cantidad de ceros es " + contadorcero + "<br>");
+    document.write("La cantidad de números pares es " + contadorpares + "<br>");
+    document.write("El promedio de positivos es " + promediopos + "<br>");
+    document.write("El promedio de negativos es " + promedioneg + "<br>");
+    document.write("La diferencia entre positivos y negativos es " + diferencia + "<br>");
+
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,24 +1,37 @@
 function mostrar() {
 
-	var contador = 0;
-	var num1 = 0;
-	var minimo
-	var maximo
-	// declarar variables
 
+	var num1;
+	var contador = 0;
+	var minimo;
+	var maximo; 
 	var respuesta = 'si';
 
 	while (respuesta == "si") {
 
-		contador++;
+		contador ++;
 
 		num1 = prompt("Ingrese un número");
 
 		num1 = parseInt(num1);
 
-		Math.max(num1);
+		if(contador == 1){//El primer número ingresado
 
-		Math.min(num1);
+		maximo = num1;
+		
+		minimo = num1;
+
+		//Entran en el else if xq a partir de aca son todos el segundo numero en adelante.
+
+		} else if(num1 > maximo){
+	 
+		maximo = num1;
+
+		} else if(num1 < minimo){
+
+		minimo = num1;
+
+		}
 
 		respuesta = prompt("si para seguir");
 
